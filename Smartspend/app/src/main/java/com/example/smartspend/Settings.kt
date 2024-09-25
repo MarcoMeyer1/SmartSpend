f0116cd6c08349063231a1f81c2dad0370cb04ae
LukeJohnson03 <lukejohnsonlgj905@gmail.com>
2024-09-25 13:23:16 +0200

    Settings Page Changes

--- Smartspend/app/src/main/java/com/example/smartspend/Settings.kt
+++ Smartspend/app/src/main/java/com/example/smartspend/Settings.kt
@@ -1,6 +1,8 @@
 package com.example.smartspend
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,21 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val spinner: Spinner = findViewById(R.id.spinner_language)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.language_array,
+            R.layout.spinner_item
+        )
+
+
+        adapter.setDropDownViewResource(R.layout.spinner_item)
+
+
+        spinner.adapter = adapter
     }
 }
+
+

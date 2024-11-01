bcfb2652d874f3a5563f61cf8520c20b08427fbf
LukeJohnson03 <lukejohnsonlgj905@gmail.com>
2024-11-01 23:53:21 +0200

    GoalDetails Page created

---
+++ Smartspend/app/src/main/java/com/example/smartspend/goal_details.kt
@@ -0,0 +1,16 @@
+package com.example.smartspend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class goal_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_goal_details)
+
+    }
+}
